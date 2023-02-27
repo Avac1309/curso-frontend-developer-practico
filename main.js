88c376e87e923daa921489391ebe274a66c964df
Jairiana de Jesus <jairianadejesus2005@gmail.com>
2023-02-27 17:35:51 -0300

    se agrego otro menu, y se modifico mucho el js

--- main.js
+++ main.js
@@ -1,17 +1,56 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
 const menuHamIcon = document.querySelector(".menu-amburguesa");
-const mobileMenu = document.querySelector(".mobile-menu")
+const mobileMenu = document.querySelector(".mobile-menu");
+const aside = document.querySelector(".product-detail");
+const menuCarritoAside = document.querySelector(".navbar-shopping-cart");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
-menuHamIcon.addEventListener("click", toggleMobileMenu)
+menuHamIcon.addEventListener("click", toggleMobileMenu);
+menuCarritoAside.addEventListener("click", toggleCarritoAside);
 
-function toggleDesktopMenu(){
-    console.log("Click en el email")
-    desktopMenu.classList.toggle("inactive")
+// //Para abrir y cerrar ventanas del meno en una solo funcion
+// function toggleDesktopMenu(){
+//     console.log("Click en el email")
+//     desktopMenu.classList.toggle("inactive")
+// }
+
+// function toggleMobileMenu() {
+//     console.log("Click al mobilMenu")
+//     mobileMenu.classList.toggle("inactive")
+// }
+
+//Para hacer que mientras se habra una ventana, la otra se cierra😳
+
+function toggleDesktopMenu() {
+    //esta abierto = se abrira
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if (!isAsideClosed) { //aqui esta cerrado
+        aside.classList.add('inactive'); //con esta se abrira 
+    }
+    
+    desktopMenu.classList.toggle('inactive');
 }
 
-function toggleMobileMenu() {
-    console.log("Click al mobilMenu")
-    mobileMenu.classList.toggle("inactive")
+function toggleMobileMenu() { 
+     //esta abierto = se abrira
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if (!isAsideClosed) {//aqui esta cerrado
+        aside.classList.add('inactive'); //con esta se abrira 
+    }
+    
+    mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoAside() {
+     //esta abierto = se abrira
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    
+    if (!isMobileMenuClosed) {//aqui esta cerrado
+        mobileMenu.classList.add('inactive'); //con esta se abrira 
+    }
+    
+    aside.classList.toggle('inactive');
 }
