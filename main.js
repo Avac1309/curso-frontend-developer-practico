32818824012958a1482a10f5ebb258dbf8bff9eb
Jairiana de Jesus <jairianadejesus2005@gmail.com>
2023-02-27 14:30:50 -0300

    Agregue al index la clase 8, ya que es el mobile menu

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
-const menuEmail = document.querySelector(".navbar-email")
-const desktopMenu = document.querySelector(".desktop-menu")
+const menuEmail = document.querySelector(".navbar-email");
+const desktopMenu = document.querySelector(".desktop-menu");
+const menuHamIcon = document.querySelector(".menu-amburguesa");
+const mobileMenu = document.querySelector(".mobile-menu")
 
-menuEmail.addEventListener("click", toggleDesktopMenu)
+menuEmail.addEventListener("click", toggleDesktopMenu);
+menuHamIcon.addEventListener("click", toggleMobileMenu)
 
 function toggleDesktopMenu(){
     console.log("Click en el email")
     desktopMenu.classList.toggle("inactive")
+}
+
+function toggleMobileMenu() {
+    console.log("Click al mobilMenu")
+    mobileMenu.classList.toggle("inactive")
 }
