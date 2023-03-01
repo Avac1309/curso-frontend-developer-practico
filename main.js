b7c2d11990811e1e69b54b88221150e5037e5472
Jairiana de Jesus <jairianadejesus2005@gmail.com>
2023-03-01 10:55:25 -0300

    Se agrego una vista del articulo en el html y java scrip, el nuevo archivo html muestra una forma diferente del articulo en JS

--- main.js
+++ main.js
@@ -5,28 +5,27 @@ const mobileMenu = document.querySelector(".mobile-menu");
 const aside = document.querySelector(".product-detail");
 const menuCarritoAside = document.querySelector(".navbar-shopping-cart");
 
-
 menuEmail.addEventListener("click", toggleDesktopMenu);
 menuHamIcon.addEventListener("click", toggleMobileMenu);
 menuCarritoAside.addEventListener("click", toggleCarritoAside);
 
 // //Para abrir y cerrar ventanas del meno en una solo funcion
 // function toggleDesktopMenu(){
-//     console.log("Click en el email")
+    //     console.log("Click en el email")
 //     desktopMenu.classList.toggle("inactive")
 // }
 
 // function toggleMobileMenu() {
-//     console.log("Click al mobilMenu")
-//     mobileMenu.classList.toggle("inactive")
-// }
-
-//Para hacer que mientras se habra una ventana, la otra se cierra😳
-
+    //     console.log("Click al mobilMenu")
+    //     mobileMenu.classList.toggle("inactive")
+    // }
+    
+    //Para hacer que mientras se habra una ventana, la otra se cierra😳
+    
 function toggleDesktopMenu() {
     //esta abierto = se abrira
     const isAsideClosed = aside.classList.contains('inactive');
-
+    
     if (!isAsideClosed) { //aqui esta cerrado
         aside.classList.add('inactive'); //con esta se abrira 
     }
@@ -35,9 +34,9 @@ function toggleDesktopMenu() {
 }
 
 function toggleMobileMenu() { 
-     //esta abierto = se abrira
+    //esta abierto = se abrira
     const isAsideClosed = aside.classList.contains('inactive');
-
+    
     if (!isAsideClosed) {//aqui esta cerrado
         aside.classList.add('inactive'); //con esta se abrira 
     }
@@ -46,10 +45,10 @@ function toggleMobileMenu() {
 }
 
 function toggleCarritoAside() {
-     //esta abierto = se abrira
+    //esta cerrado = se abrira
     const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
     
-    if (!isMobileMenuClosed) {//aqui esta cerrado
+    if (!isMobileMenuClosed) { //aqui esta cerrado
         mobileMenu.classList.add('inactive'); //con esta se abrira 
     }
     
@@ -60,46 +59,64 @@ function toggleCarritoAside() {
 const productList = []; //estos son los productos que apareceran 
 productList.push({
     name: "Bike",
-    price: 130,
-    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+    price: 500,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    id: "product-card-image-article",
 })
 productList.push({
     name: "Computadora",
     price: 500,
-    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    id: "product-card-image-article",
 })
 productList.push({
     name: "Pantalla",
     price: 230,
-    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    id: "product-card-image-article",
 })
 productList.push({
     name: "Monitor",
     price: 900,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    id: "product-card-image-article",
 })
 
 function renderProducts(productList) {
     for (product of productList) {
-
-        const htmlCards =  `<div class="product-card">
-            <img src= ${product.image} alt="">
-            <div class="product-info">
-                <div>
-                    <p>$${product.price}</p>
-                    <p>${product.name}</p>
-                </div>
+        
+        const htmlCards = `<div class="product-card">
+        <img src= ${product.image} alt="" id=${product.id}>
+        <div class="product-info">
+        <div>
+            <p>$${product.price}</p>
+            <p>${product.name}</p>
+            </div>
                 <figure>
                     <img src="./icons/bt_add_to_cart.svg" alt="">
                 </figure>
             </div>
         </div>  `
-
+                    
         const cardsContainer = document.querySelector(".cards-container");
         cardsContainer.innerHTML += htmlCards
     }
 }
-
 renderProducts(productList);
 
+// Click en el image 
+const funcionnImage = document.querySelector("#product-card-image-article");
+const productDetailSecondary = document.querySelector(".product-detail-secondary")
+const productDetailSecondaryClose = document.querySelector(".product-detail-secondary-close");
 
+funcionnImage.addEventListener("click", openFuncionImagen);
+productDetailSecondaryClose.addEventListener("click", closeFuncionImage)
+
+function openFuncionImagen() {
+    console.log("cLick en el image")
+        productDetailSecondary.classList.remove("inactive")
+}
+
+function closeFuncionImage() {
+    productDetailSecondary.classList.add("inactive")
+}
